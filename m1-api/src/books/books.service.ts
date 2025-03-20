9b207b89e4a4e91f3ab0236212df4e0e64da15d8
Thorgrim <fordelonemarco@orange.fr>
2025-03-20 17:04:40 +0100

    books back

---
+++ m1-api/src/books/books.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Book } from './book.entity';
+
+@Injectable()
+export class BooksService {
+  constructor(
+    @InjectRepository(Book)
+    private booksRepository: Repository<Book>,
+  ) {}
+
+  findAll(): Promise<Book[]> {
+    return this.booksRepository.find();
+  }
+
+  findOne(id: number): Promise<Book | null> {
+    return this.booksRepository.findOneBy({ id });
+  }
+
+  create(book: Book): Promise<Book> {
+    return this.booksRepository.save(book);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.booksRepository.delete(id);
+  }
+}
